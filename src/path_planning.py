c63f3153590a6da6afe1fae0769e6e44bc3edd5f
amadou <abah@mit.edu>
2020-04-20 05:15:28 -0400

    Path follower speed up

--- src/path_planning.py
+++ src/path_planning.py
@@ -2,6 +2,7 @@
 
 import rospy
 import numpy as np
+import tf
 from scipy import signal 
 import matplotlib.pyplot as plt
 import Queue
@@ -16,17 +17,23 @@ class PathPlan(object):
     current car pose.
     """
     def __init__(self):
-        # Fields
-        self.goal_pos = None
-        self.start_pos = None
-        self.g_map = None
-        self.map_x_offset = None
-        self.map_y_offset = None
-        self.map_resolution = None
-        #Euler angnles:
-        sel
+
+        # FIELDS
+        #self.goal_pos = None
+        #self.start_pos = None
+        #self.g_map = None
+        #self.map_x_offset = None
+        #self.map_y_offset = None
+        #self.map_resolution = None
+
+        # Euler angnles:
+        #self.yaw = 0
+        #self.pitch = 0
+        #self.roll = 0
+        #self.CCW_rotation_matrix = []
+        #self.CW_rotation_matrix = []
         
-        # Subscribers
+        # SUBSCRIBERS and PUBLISHERS
         self.odom_topic = rospy.get_param("~odom_topic")
         self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
         self.trajectory = LineTrajectory("/planned_trajectory")
@@ -41,42 +48,78 @@ class PathPlan(object):
         self.map_y_offset = msg.info.origin.position.y
         self.map_resolution = msg.info.resolution
 
-        # Treat some spots as certain obstacles
-        self.g_map = np.where(data < 0, 100, data)
-        self.g_map = np.where(self.g_map > 10, 100, self.g_map)
-        # VISUALIZING THE MAP
+        # Obtaining the Euler angles and rotation matrix
+        x = msg.info.origin.orientation.x
+        y = msg.info.origin.orientation.y
+        z = msg.info.origin.orientation.z
+        w = msg.info.origin.orientation.w
+        (self.roll, self.pitch, self.yaw) = tf.transformations.euler_from_quaternion([x,y,z,w])
+        self.CCW_rotation_matrix = np.array([[np.cos(self.yaw),-np.sin(self.yaw)],[np.sin(self.yaw),np.cos(self.yaw)]])
+        self.CW_rotation_matrix = np.array([[np.cos(self.yaw),np.sin(self.yaw)],[-np.sin(self.yaw),np.cos(self.yaw)]])
+        print("THIS IS THE YAW => "+str(self.yaw))
+        print("THIS IS THE PITCH => "+str(self.pitch))
+        print("THIS IS THE ROLL => "+str(self.roll))
+
+        # Treat some spots as certain obstacles - May not need this
+        #self.g_map = data
+        self.g_map = np.where(data != 0, 100, data)
+        #self.g_map = np.where(self.g_map > 0, 100, self.g_map)
+
+        # Dialating the map using scipy
+        #kernel = np.array([[1,1,1],[1,1,1],[1,1,1]])*(1/9)
+        #self.g_map = signal.convolve2d(self.g_map, kernel, boundary='fill', mode='same')
+
+        # VISUALIZING THE MAP - only for debugging
         plt.imshow(self.g_map)
         plt.show()
-        # Dialating the map using scipy
-        kernel = np.array([[1,1,1],[1,1,1],[1,1,1]])*(1/9)
-        self.g_map = signal.convolve2d(self.g_map, kernel, boundary='fill', mode='same')
+
         
-
-
-
-        self.x_origin = msg.info.origin.position.x
-        self.y_origin = msg.info.origin.position.y
-        self.resolution = msg.info.resolution
+    
         
     def odom_cb(self, msg):
-        self.start_pos = ((msg.pose.pose.position.x-self.map_x_offset)/self.map_resolution, (msg.pose.pose.position.y-self.map_y_offset)/self.map_resolution)
-        #print("start:" + str((msg.pose.pose.position.x, msg.pose.pose.position.y)))
+        x_0 = msg.pose.pose.position.x
+        y_0 = msg.pose.pose.position.y
+        self.start_pos = (x_0,y_0)
 
     def goal_cb(self, msg):
+        x_0 = msg.pose.position.x
+        y_0 = msg.pose.position.y
+        self.goal_pos = (x_0,y_0) 
         
-        self.goal_pos = ((msg.pose.position.x-self.map_x_offset)/self.map_resolution, (msg.pose.position.y-self.map_y_offset)/self.map_resolution) 
-        
-        #print("end: " + str((msg.pose.position.x, msg.pose.position.y)))
         # Only call the path planned once a goal position has been specified
         self.plan_path(self.start_pos, self.goal_pos, self.g_map)
 
     def plan_path(self, start_position, goal_position, ground_map):
         rospy.loginfo("Building the path...")
-        # Making sure that we start from and end at a integer positions
-        # TODO: May need to fix this later
-        start = (int(start_position[0]),int(start_position[1]))
-        goal = (int(goal_position[0]), int(goal_position[1]))
+        # ADJUSTMENT
+        #start
+        x_0 = start_position[0]
+        y_0 = start_position[1]
+        rotated = np.dot(self.CCW_rotation_matrix,np.array([[x_0],[y_0]]))
+        x_r = rotated[0][0]
+        y_r = rotated[1][0]
+        x = (x_r+self.map_x_offset)/self.map_resolution
+        y = (y_r+self.map_y_offset)/self.map_resolution
+        start_d = (x,y)
         
+        #goal
+        x_0 = goal_position[0]
+        y_0 = goal_position[1]
+        rotated = np.dot(self.CCW_rotation_matrix,np.array([[x_0],[y_0]]))
+        x_r = rotated[0][0]
+        y_r = rotated[1][0]
+
+        x = (x_r+self.map_x_offset)/self.map_resolution
+        y = (y_r+self.map_y_offset)/self.map_resolution
+        goal_d = (x,y)
+
+        
+        start = (int(start_d[0]),int(start_d[1]))
+        goal = (int(goal_d[0]), int(goal_d[1]))
+        
+        print("start = " + str(start))
+        print("goal = "+ str(goal))
+
         # Using L-2 norm distance... maybe manhatan would be better?
         def heuristic(location):
             return ((location[0]-goal[0])**2+(location[1]-goal[1])**2)**(1/2)
@@ -84,20 +127,20 @@ class PathPlan(object):
         # Reconstruct the path using parent pointers
         def build_path(came_from):
             path = []
-            #if goal != goal_position:
-            path.append(((goal_position[0]*self.map_resolution)+self.map_x_offset, (goal_position[1]*self.map_resolution)+self.map_y_offset))
+            # Fix position
+            path.append(goal_position)
             position = came_from[goal]
             while came_from[position] is not None:
-                path.append(((position[0]*self.map_resolution)+self.map_x_offset,(position[1]*self.map_resolution)+self.map_y_offset))
+                x_0 = (position[0]*self.map_resolution)-self.map_x_offset
+                y_0 = (position[1]*self.map_resolution)-self.map_y_offset
+                rotated = np.dot(self.CW_rotation_matrix,np.array([[x_0],[y_0]]))
+                x = rotated[0][0]
+                y = rotated[1][0]
+                path.append((x,y))
+                #path.append(((position[0]*self.map_resolution)+self.map_x_offset,(position[1]*self.map_resolution)+self.map_y_offset))
                 position = came_from[position]
-            #path.append(start)
-            #if start != start_position:
-            path.append(((start_position[0]*self.map_resolution)+self.map_x_offset, (start_position[1]*self.map_resolution)+self.map_y_offset))
+            path.append((start_position))
             path.reverse()
-            
-            #fix resolution and add back offset
-            #path = [((i[0]*self.map_resolution)+self.map_x_offset, (i[1]*self.map_resolution)+self.map_y_offset) for i in path]
-
             self.trajectory.points = path
             #rospy.loginfo(str(path))
             return
@@ -125,8 +168,10 @@ class PathPlan(object):
         # The main part
         #while not open_queue.empty():
         while len(open_set) != 0:
+            #print("Searching...")
             inter = open_set.intersection(rev_f_score[min(rev_f_score)])
             current = inter.pop()
+            #rospy.loginfo("Current = " + str(current))
             #current = open_queue.get()
             if current == goal:
                 build_path(came_from)
@@ -151,16 +196,16 @@ class PathPlan(object):
                     neighbors.append((current[0], current[1]-1))
                 
                 #DIAGONALS
-                #Top right
+                #Bottom right
                 if current[0]+1 <= H-1 and current[1]+1 <= W-1:
                     neighbors.append((current[0]+1, current[1]+1))
-                #Top left
+                #Bottom left
                 if current[0]+1 <= H-1 and current[1]-1 >= 0:
                     neighbors.append((current[0]+1, current[1]-1))
-                #Bottom right
+                #Top right
                 if current[0]-1 >= 0 and current[1]+1 <= W-1:
                     neighbors.append((current[0]-1, current[1]+1))
-                #Bottom left
+                #Top left
                 if current[0]-1 >= 0 and current[1]-1 <= 0:
                     neighbors.append((current[0]-1, current[1]-1))
                 
@@ -171,7 +216,7 @@ class PathPlan(object):
             for each in neighbors:
                 tentative_score = g_score[current] + 1
                 # Treating anything with probability higher than 10 as a certain wall
-                if ground_map[current[0]][current[1]] == 0 and (each not in g_score or tentative_score <= g_score[each]):
+                if ground_map[current[1]][current[0]] == 0  and (each not in g_score or tentative_score <= g_score[each]):
                     g_score[each] = tentative_score
                     came_from[each] = current
                     # optimize this part by creating an h = {} and not repeating calculations
